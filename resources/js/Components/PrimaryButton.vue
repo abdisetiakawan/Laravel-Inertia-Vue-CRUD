73f3b61f14b755424c0258c50f045fc0694d38ff
abdisetiakawan <bedikadiryt@gmail.com>
2025-06-02 11:29:43 +0700

    feat: Adds secondary button component
    Adds a new secondary button component with distinct styling.
    
    Improves the primary button by adding a hover effect.

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        class="px-6 py-2 rounded-lg bg-slate-950 text-white disabled:bg-slate-300 disabled:cursor-wait"
+        class="px-6 py-2 rounded-lg bg-slate-950 text-white disabled:bg-slate-300 hover:bg-slate-900 disabled:cursor-wait"
     >
         <slot />
     </button>
---
+++ resources/js/Components/SecondaryButton.vue
@@ -0,0 +1,7 @@
+<template>
+    <button
+        class="px-6 py-2 rounded-lg bg-gray-200 text-gray-800 hover:bg-gray-300 dark:bg-gray-700 dark:text-gray-200 dark:hover:bg-gray-600 disabled:opacity-50 disabled:cursor-wait"
+    >
+        <slot />
+    </button>
+</template>
